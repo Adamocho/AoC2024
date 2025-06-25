d7eaf5a686022ab6098b54ce3bfbe4fce01e6825
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-06-25 11:43:08 +0200

    Change input to JSON

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+
+    Ok(())
 }
