cd00f0e35e708668109e1c9109aa3a6af19b2bce
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-06-28 20:19:11 +0200

    Start day10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let lines = match fs::read_to_string("input") {
+    // let lines = match fs::read_to_string("example") {
+        Ok(x) => x,
+        Err(_) => panic!("Couldn't read from file"),
+    };
+
+    let mut trail_map: Vec<Vec<u32>> = vec![];
+
+    lines
+        .split("\n")
+        .for_each(
+            |row|  {
+                trail_map.push(
+                    row
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap())
+                    .collect::<Vec<u32>>()
+                )
+            }
+        );
+
+    dbg!(trail_map);
 }
