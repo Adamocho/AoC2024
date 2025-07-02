312d4e2445e37c095370a9cfea2dc62b5fccb034
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-07-02 22:46:21 +0200

    Change code for day11 - proritize quickness over neat code

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,87 +1,70 @@
-use core::num;
-use std::{collections::{HashMap, LinkedList, VecDeque}, fs, hash::Hash, vec};
-
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-
-fn blink(cache: &mut HashMap<u64, u64>) {
-    let mut length: usize;
-    let mut operand: u64;
-
-    // let numer_of_zeros = cache.entry(0).or_insert(0);
-    let mut inserting: Vec<(u64, u64)> = vec![];
-    let mut make_zero: Vec<u64> = vec![];
-
-    cache.keys().for_each(|key| {
-        if cache[key] == 0 || *key == 0 {
-            return;
-        }
-        let length = (*key as f64).log10().floor() as usize + 1;
-        let operand = 10_u64.pow(length as u32/ 2);
-        if length % 2 == 0 {
-            inserting.push((key / operand, *cache.get(key).unwrap()));
-            inserting.push((key % operand, *cache.get(key).unwrap()));
-            make_zero.push(*key);
-        } else {
-            inserting.push((key * 2024, *cache.get(key).unwrap()));    
-            make_zero.push(*key);
-        }
-    });
-
-    let number = cache[&0];
-    // remove 0s
-    cache.entry(0).and_modify(|value| { *value = 0 }).or_insert(0);
-
-    // zero divided stones
-    for stone in make_zero {
-        cache.entry(stone).and_modify(|value| { *value = 0 });
-    }
-
-    // update divided stones
-    for stone in inserting {
-        cache.entry(stone.0).and_modify(|value| { *value += stone.1 }).or_insert(stone.1);
-    }
-
-    // update all 1-s
-    cache.entry(1).and_modify(|value| { *value += number }).or_insert(number);
-}
+use core::panic;
+use std::{collections::{HashMap, HashSet}, fs};
 
 fn main() {
-    // let lines = match fs::read_to_string("example") {
     let lines = match fs::read_to_string("input") {
-        Ok(x) => x,
-        Err(e) => panic!("Could not access file: {}", e),
+        Ok(value) => value,
+        Err(e) => panic!("Could not read file: {}", e)
     };
 
-    let mut test = HashMap::new();
-    *test.entry("some").or_insert(10) *= 10;
+    let mut stones: Vec<u64> = lines.split(" ").map(|stone| stone.parse::<u64>().unwrap()).collect();
 
-    let stones_strings: Vec<&str> = lines.trim().split(" ").collect();
-    let stones: Vec<u64> = stones_strings.iter().map(|value| value.parse().unwrap()).collect();
-
-    let mut cache: HashMap<u64, u64> = HashMap::new();
-
-    stones.iter().for_each(|stone| {
-        *cache.entry(*stone).or_insert(0) += 1;
-    });
-
+    let mut map: HashMap<u64, u64> = HashMap::new();
     let mut counter = 0;
-    // make sure zero is there
-    let _ = cache.entry(0).or_insert(0);
+    let mut to_add: Vec<(u64, u64)>;
+    let mut to_zero: Vec<u64>;
 
-    while counter < 75 {
-        blink(&mut cache);
-        counter += 1;
-        dbg!(counter);
+    for stone in &stones {
+        map.entry(*stone).and_modify(|value| {*value += 1}).or_insert(1);
     }
 
-    // dbg!(&cache);
+    // dbg!(&stones);
 
-    // cache.iter().for_each(|(x, y)| {
-    //     let repeated = (x.to_string() + " ").repeat(*y as usize);
-    //     print!("{}", repeated);
-    // });
-    
-    let sum: u64 = cache.par_iter().map(|(_, y)| *y).sum();
+    map.entry(0).or_insert(0);
+
+    while  counter < 75 {
+        to_add = vec![];
+        to_zero = vec![];
+        for stone in map.keys() {
+            let length: u32= (*stone as f64).log10().floor() as u32 + 1;
+            let power: u64 = 10_u64.pow(length / 2);
+            
+            if *stone == 0 {
+                continue;
+            }
+            else if length % 2 == 0 {
+                to_add.push((*stone % power, map[stone]));
+                to_add.push((*stone / power, map[stone]));
+                to_zero.push(*stone);
+            } else {
+                to_add.push((*stone * 2024, map[stone]));
+                to_zero.push(*stone);
+            }
+        }
+
+        // reset 0s
+        let zeros = map[&0];
+        map.entry(0).and_modify(|value| {*value = 0});
+
+        for key in to_zero {
+            map.entry(key).and_modify(|value| {*value = 0});
+        }
+
+        for (key, quantity) in to_add {
+            map.entry(key).and_modify(|value| {*value += quantity}).or_insert(quantity);
+        }
+
+        // add 1s
+        map.entry(1).and_modify(|value| {*value += zeros}).or_insert(zeros);
+
+        counter += 1;
+        // dbg!(counter);
+        // dbg!(&map);
+    }
+
+    // dbg!(&stones);
+
+    let sum: u64 = map.iter().map(|(_, value)| value).sum();
     dbg!(sum);
-    
+
 }
