5a012eab9312f5245f2193740ef9dc17bbcc2a73
Adamocho <81510507+Adamocho@users.noreply.github.com>
2025-08-28 18:07:34 +0200

    Work

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,176 @@
+use std::ops::{Add, Neg};
+
+
+struct Game {
+    map: Vec<Vec<char>>,
+    robot: Vector2D,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Vector2D {
+    x: i32,
+    y: i32
+}
+
+impl Add for Vector2D {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector2D {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
+        }
+    }
+}
+
+impl Neg for Vector2D {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Vector2D {
+            x: -self.x,
+            y: -self.y
+        }
+    }
+}
+
+enum  Instruction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+const MAP_STRING: &str =
+"########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########";
+
+const INSTRUCTIONS: &str = "<^^>>>vv<v>>v<<";
+
+impl Game {
+    fn execute_next_cycle(&mut self, instruction: Instruction) {
+        // let instruction = &self.instruction[0];
+        let direction: Vector2D;
+        match instruction {
+            Instruction::UP => direction = Vector2D { x: 0, y: -1 },
+            Instruction::RIGHT => direction = Vector2D { x: 1, y: 0 },
+            Instruction::DOWN => direction = Vector2D { x: 0, y: 1 },
+            Instruction::LEFT => direction = Vector2D { x: -1, y: 0 },
+        }
+        let future_coords = Vector2D {
+            y: self.robot.y + direction.y,
+            x: self.robot.x + direction.x,
+        };
+        let point = &self.map
+        [future_coords.y as usize]
+        [future_coords.x as usize];
+
+        if *point == '#' {
+            return
+        }
+        if *point == '.' {
+            self.map[self.robot.y as usize][self.robot.x as usize] = '.';
+            self.map[future_coords.y as usize][future_coords.x as usize] = '@';
+            return
+        }
+        if *point == 'O' {
+            self.find_free_space(future_coords, direction)
+        }
+    }
+
+    fn find_free_space(&mut self, position: Vector2D, direction: Vector2D) {
+        let c = self.map[position.y as usize][position.x as usize];
+        if c == '#' {
+            return
+        }
+        if c == '.' {
+            // move them
+            self.move_objects(position, self.robot, -direction);
+            return
+        }
+        if c == 'O' {
+            self.find_free_space(position + direction, direction);
+        }
+    }
+
+    fn move_objects(&mut self, start: Vector2D, end: Vector2D, direction: Vector2D) {
+        if start.x == end.x && start.y == end.y {
+            return
+        }
+
+        let pointer = start;
+        let temp: char = self.map[pointer.y as usize][pointer.x as usize];
+        self.map[pointer.y as usize][pointer.x as usize] =
+            self.map[(pointer.y + direction.y) as usize][(pointer.x + direction.x) as usize];
+        self.map[(pointer.y + direction.y) as usize][(pointer.x + direction.x) as usize] = temp; 
+
+        self.move_objects(start + direction, end, direction)
+    }
+}
+
+
+fn count_score(map: &Vec<Vec<char>>) -> usize {
+    let mut sum = 0;
+
+    for x in 0..map.len() {
+        for y in 0..map[0].len() {
+            if map[x][y] == 'O' {
+                sum += 100 * y + x;
+            }
+        }
+    }
+   sum 
+}
+
+fn show_map(map: &Vec<Vec<char>>) {
+    let mut line;
+    for x in 0..map.len() {
+        line = String::new();
+        for y in 0..map[0].len() {
+            line.push(map[x][y]);
+        }
+        println!("{}", line);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut map: Vec<Vec<char>> = vec![];
+
+    for line in MAP_STRING.lines() {
+        map.push(line.chars().collect());
+    }
+
+    show_map(&map);
+
+    let mut instructions: Vec<Instruction> = vec![];
+    for instruction in INSTRUCTIONS.chars() {
+        instructions.push(
+            match instruction {
+            '>' => Instruction::RIGHT,
+            'v' => Instruction::DOWN,
+            '<' => Instruction::LEFT,
+            '^' => Instruction::UP,
+            _ => panic!("unexpected token")
+        });
+    }
+
+    let mut game = Game {
+        map: map,
+        robot: Vector2D { x: 2, y: 2 },
+    };
+
+    for instruction in instructions {
+        game.execute_next_cycle(instruction);
+        show_map(&game.map);
+    }
+
+    let score = count_score(&game.map);
+    dbg!(score);
 }
